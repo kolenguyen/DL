cb75a33aabad126eddc73d85ea61e560ad50b40a
arjunmalik11 <142700188+arjunmalik11@users.noreply.github.com>
2023-10-19 19:32:50 -0400

    Update app.py

--- env/app.py
+++ env/app.py
@@ -4,9 +4,6 @@ app = Flask(__name__)
 
 
 @app.route("/")
-# printing out a statement
-# def home():
-#     return "Hello, Flask!"
-# Look for a file called index.html from templates folder
+
 def index():
     return render_template('index.html')
