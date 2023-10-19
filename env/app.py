19640aa7215100896570fd682ee4e690f7f23dca
arjunmalik11 <malia01@pfw.edu>
2023-10-19 13:03:12 -0400

    Created models in Flask

--- env/app.py
+++ env/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-#printing out a statement
+# printing out a statement
 # def home():
 #     return "Hello, Flask!"
-
 # Look for a file called index.html from templates folder
 def index():
     return render_template('index.html')
