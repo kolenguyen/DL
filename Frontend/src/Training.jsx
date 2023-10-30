404945e35645bbb2fd537a3c51a3b007fdd3bc14
fermerida <fer.merida94@gmail.com>
2023-10-29 21:19:04 -0400

    fixed identation and active nav issues

--- Frontend/src/Training.jsx
+++ Frontend/src/Training.jsx
@@ -7,16 +7,16 @@ import Grid from '@mui/material/Grid';
 export default function Training() {  
     return (
     <div style={{margin: 0}}>
-        <h2>Training</h2>
-        <Grid container spacing={26}>
+      <h2>Training</h2>
+      <Grid container spacing={26}>
         <Grid item xs={6}>
           <ElementList />
         </Grid>
-        <Grid item xs={6} style={{ textAlign: 'right' }}>
+        <Grid item xs={6} style={{ textAlign: 'right'}}>
           <VideoComponent />
         </Grid>
       </Grid>
-      </div>
+    </div>
     )
   }
   
