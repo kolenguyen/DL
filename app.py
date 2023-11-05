abd75dadf595713e79e1ef84603f6c505601a82b
kole <khoa.fsu@gmail.com>
2023-11-05 14:50:09 -0500

    register, login api

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from models import User, db
 from serializers import UserSerializer, ma
+from marshmallow import ValidationError
+from sqlalchemy.orm.exc import NoResultFound
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
 
 #initialize app and configurate based on config.py
 #config.py is based on .flaskenv
@@ -19,20 +23,12 @@ us = UserSerializer()
 migrate = Migrate(app,db)
 
 # ma = Marshmallow(app)
+jwt=JWTManager(app)
 bcrypt = Bcrypt(app)
 api = Api(app)
 CORS(app)
 
 @app.route('/')
-def home():
-    # db.drop_all()
-    # db.create_all()
-    new_user = User(username="john6", email="john6@example.com", password="john")
-    db.session.add(new_user)
-    db.session.commit()
-    us.dump(new_user)
-    response_body = {}
-    return response_body
 
 @app.route("/user", methods=["GET"])
 def getAllUsers():
@@ -42,3 +38,56 @@ def getAllUsers():
     # print(us.validate(allUsers))
     print(results)
     return jsonify(results)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    try:
+        data['password'] = generate_password_hash(data['password'])
+        newUser = User(username=data['username'], email=data['email'], password=data['password'])
+    except ValidationError as err:
+        return jsonify({
+            'message':'Invalid input data',
+            'errors': err.messages
+        }), 400
+    
+    db.session.add(newUser)
+    db.session.commit()
+    return jsonify({'message':'User added successfuly'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    loginSerializer = UserSerializer(only=("username", "password"))
+
+    #validate data 
+    try:
+        loginData = loginSerializer.load(data)
+    except ValidationError as err:
+        return jsonify({
+            'message': 'invalid input data',
+            'errors': err.messages
+        }), 400
+
+    #check username and password
+    username = loginData.get('username')
+    password = loginData.get('password')
+
+    try:
+        user= db.session.query(User).filter(User.username == username).one()
+    except NoResultFound:
+        return jsonify({'message':'User not found'}), 404
+    
+    if check_password_hash(user.password,password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'message':'Login succesful', 'access_token': access_token}), 200
+    else: 
+        return jsonify({'message':'Invalid password'}), 401
+   
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    return jsonify({'message':'Testing authentication', 'user_id': current_user_id})
