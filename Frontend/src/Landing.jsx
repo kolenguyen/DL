85c606953eb33cd95bfea77540e27e8a2ee82ae4
kole <khoa.fsu@gmail.com>
2023-12-01 23:57:03 -0500

    change some css

--- Frontend/src/Landing.jsx
+++ Frontend/src/Landing.jsx
@@ -10,14 +10,21 @@ export default function Landing() {
         muted loop autoPlay>
         </video>
         <header className="landingtext">
-            <h2>ASL Translator</h2>
+            <h2>ASL TRANSLATOR</h2>
             <h3>Translate and learn ASL language.</h3>
-            <ul>
+            {/* <ul>
                 <li>Group 2</li>
                 <li>Members:</li>
                 <li>Build using React, MUI and python</li>
+            </ul> */}
+
+            <ul>
+                <p>Deep learning project using Python, Flask and React</p>
+                <p>Made by Kole, Fernando, Yashi and Arjun</p>
             </ul>
-            <Button variant="contained" href='/login'>Contained</Button>        
+
+            <Button variant="contained" href='/login'>Login</Button>      
+            {/* <Button variant="contained" path="/practice" element={<Practice/>}>Register</Button>     */}
             </header>
     </main>
       </div>
